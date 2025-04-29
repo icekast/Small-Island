fd15ab56785dc54bed924f46232d219fb4fcf5af
ibinafol <146542141+ibinafol@users.noreply.github.com>
2025-04-30 02:19:06 +0300

    сцена меню и чет звук хз

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Метод для перехода на другую сцену
+    public void LoadScene(string MainScene)
+    {
+        SceneManager.LoadScene(MainScene);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
